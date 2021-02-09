5118df3756713733c28e968487e03d4352056ac7
Tekko2000 <ot1977@gmx.net>
2021-02-09 21:06:04 +0100

    JS DOM lvl 2.2

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var vorname = document.getElementById('vorname');
+var nachname = document.getElementById('nachname');
+var land = document.getElementById('land');
+
+function data() {
+    console.log("fullname: " + vorname.value + " Nachname: " + nachname.value + " Land: " + land.value);
+}
+
